c1be49d44d2f89fa215f58fd8e60223ac4ef1584
BHALERAO PAVAN <143244268+baleraopavankumar@users.noreply.github.com>
2026-02-05 07:46:28 +0530

    Initialize SchedulerApplication with Spring Boot

---
+++ SchedulerApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+@EnableScheduling
+public class SchedulerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerApplication.class, args);
+    }
+}
